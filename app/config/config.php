5222bf5e630b7eb04a2305db6063ec3e7f360921
glifordosade <glifordosade@gmail.com>
2024-06-09 11:29:17 +0700

    update komen

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,12 @@
 <?php
 
+// membuat constanta dari link website
 define('BASEURL','http://zalora.test:8080');
+
+// membuat constanta untuk melakukan koneksi ke database
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','pwebzalora');
 
 
-// config/config.php
-// define('SESSION_LIFETIME', 120); // 30 menit
-
-// // Set session lifetime
-// ini_set('session.gc_maxlifetime', SESSION_LIFETIME);
-// session_set_cookie_params(SESSION_LIFETIME);
